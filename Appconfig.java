4d36f3bdaac07619216826b013d3bae081c7a1c9
Pabita Allada <pxa06120@ucmo.edu>
2017-01-18 04:15:26 -0600

    Add files via upload
    Target-class files
    src-Java files
    .settings-eclipse packages

---
+++ Appconfig.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package io.projects.springcore;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ComponentScan;
+
+@Configuration 
+@ComponentScan
+public class Appconfig {
+
+	
+}
